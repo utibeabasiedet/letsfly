86742fa5672e10f4f41a03acac4febaa35785da9
utibe edet <utibeabasi.okon@myrecharge.ng>
2023-10-16 14:14:15 -0700

    new

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -47,34 +47,34 @@ function Page() {
   const [pageLoaded, setPageLoaded] = useState(false);
   const currentUser = getUser();
 //   console.log(currentUser)
-  const { userId } = currentUser;
-  useEffect(() => {
-    // Perform an API call
-    get_transactions( userId)
-      .then((response) => {
-        // Data has been successfully fetched, so set pageLoaded to true
-        setPageLoaded(true);
-      })
-      .catch((error) => {
-        // Handle the error here, and you may set pageLoaded to false if needed.
-        setPageLoaded(false);
-      });
-  }, []);
+  // const { userId } = currentUser;
+  // useEffect(() => {
+  //   // Perform an API call
+  //   get_transactions( userId)
+  //     .then((response) => {
+  //       // Data has been successfully fetched, so set pageLoaded to true
+  //       setPageLoaded(true);
+  //     })
+  //     .catch((error) => {
+  //       // Handle the error here, and you may set pageLoaded to false if needed.
+  //       setPageLoaded(false);
+  //     });
+  // }, []);
 
-  if (!pageLoaded) {
-    return (
-      <div className="flex flex-col justify-center items-center h-[80vh]">
-        <div className="relative w-32 h-32">
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div className="w-16 h-16 bg-blue-500 rounded-full animate-ping"></div>
-            <div className="w-16 h-16 bg-blue-300 rounded-full animate-pulse"></div>
-          </div>
-          <div className="w-32 h-32 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
-        </div>
-        <p className="mt-4 text-xl font-semibold text-blue-500">Loading...</p>
-      </div>
-    );
-  }
+  // if (!pageLoaded) {
+  //   return (
+  //     <div className="flex flex-col justify-center items-center h-[80vh]">
+  //       <div className="relative w-32 h-32">
+  //         <div className="absolute inset-0 flex items-center justify-center">
+  //           <div className="w-16 h-16 bg-blue-500 rounded-full animate-ping"></div>
+  //           <div className="w-16 h-16 bg-blue-300 rounded-full animate-pulse"></div>
+  //         </div>
+  //         <div className="w-32 h-32 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+  //       </div>
+  //       <p className="mt-4 text-xl font-semibold text-blue-500">Loading...</p>
+  //     </div>
+  //   );
+  // }
 
   return (
     <main className="">
